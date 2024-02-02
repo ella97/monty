cc4a5f0e268fd4888c7c91f8d2dd2b9a74d5975e
ella97 <nuwymwanri@gmail.com>
2024-02-02 14:12:19 +0300

    .\operations_instructions_code.c

--- operations_instructions_code.c
+++ operations_instructions_code.c
@@ -1,62 +1,62 @@
 #include "monty.h"
 
-/*
- * _push - pushes an element to the stack
- * @stack: stack
- * @line_number: line number
- * Return: void
- * you can assume it will be used in a doubly linked list
- */
+/**
+* _push - pushes an element to the stack
+* @stack: stack
+* @line_number: line number
+* Return: void
+* you can assume it will be used in a doubly linked list
+*/
 
 void _push(stack_t **stack, unsigned int line_number)
 {
-    stack_t *new;
+stack_t *new;
 
-    if (*stack == NULL)
+if (*stack == NULL)
+{
+    new = malloc(sizeof(stack_t));
+    if (new == NULL)
     {
-        new = malloc(sizeof(stack_t));
-        if (new == NULL)
-        {
-            fprintf(stderr, "Error: malloc failed\n");
-            exit(EXIT_FAILURE);
-        }
-        new->n = line_number;
-        new->next = NULL;
-        new->prev = NULL;
-        *stack = new;
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
     }
-    else
+    new->n = line_number;
+    new->next = NULL;
+    new->prev = NULL;
+    *stack = new;
+}
+else
+{
+    new = malloc(sizeof(stack_t));
+    if (new == NULL)
     {
-        new = malloc(sizeof(stack_t));
-        if (new == NULL)
-        {
-            fprintf(stderr, "Error: malloc failed\n");
-            exit(EXIT_FAILURE);
-        }
-        new->n = line_number;
-        new->next = *stack;
-        new->prev = NULL;
-        (*stack)->prev = new;
-        *stack = new;
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
     }
+    new->n = line_number;
+    new->next = *stack;
+    new->prev = NULL;
+    (*stack)->prev = new;
+    *stack = new;
+}
 }
 
-/*
- * _pall - prints all the elements of the stack
- * @stack: stack
- * @line_number: line number
- * Return: void
- * you can assume it will be used in a doubly linked list
- */
+/**
+* _pall - prints all the elements of the stack
+* @stack: stack
+* @line_number: line number
+* Return: void
+* you can assume it will be used in a doubly linked list
+*/
 
 void _pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp;
+stack_t *temp;
 
-    temp = *stack;
-    while (temp != NULL)
-    {
-        printf("%d\n", temp->n);
-        temp = temp->next;
-    }
+temp = *stack;
+while (temp != NULL)
+{
+    printf("%d\n", temp->n);
+    temp = temp->next;
+}
 }
